a483c6a6f8e30cb8acfebcf99701e8d6818303a1
morningstar02 <morningstar.lux11@gmail.com>
2019-04-04 02:54:45 +0530

    Auth API - Stage 1

---
+++ models/user.model.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+var userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: 'Full name can\'t be empty'
+    },
+    email: {
+        type: String,
+        required: 'Email can\'t be empty',
+        unique: true
+    },
+    password: {
+        type: String,
+        required: 'Password can\'t be empty',
+        minlength: [4, 'Password must be atleast 4 characters long']
+    },
+    saltSecret: String
+});
+
+// Custom validation for email using regex
+userSchema.path('email').validate((val) => {
+    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(val);
+}, 'Invalid e-mail.');
+
+userSchema.pre('save', function (next) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            this.password = hash;
+            this.saltSecret = salt;
+            next();
+        });
+    });
+});
+
+//Methods
+userSchema.methods.verifyPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+userSchema.methods.generateJwt = function() {
+    const today = new Date();
+    const expirationDate = new Date(today);
+    expirationDate.setDate(today.getDate() + 60);
+
+    return jwt.sign({
+        _id: this.id,
+        email: this.email,
+        exp: parseInt(expirationDate.getTime() / 1000, 10),
+    }, process.env.JWT_SECRET);
+}
+
+mongoose.model('User', userSchema);
